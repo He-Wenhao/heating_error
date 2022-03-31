ecefe478bdd6c660cb2e790433b3534c7d6d432f
tenplayers <20010509@qq.com>
2022-03-31 14:26:53 +0800

    first picture down

--- main.py
+++ main.py
@@ -7,12 +7,29 @@ import time
 from hzx_amp.wave_pulse import hzx_amp
 from syc_amp.wave_pulse import syc_amp
 from zxm_freq.Tab1_ion_pos_spec_cal import  radial_mode_spectrum
-import read_amp
+import read_data
 import scipy.integrate as integrate
+import sympy as sp
+from functools import lru_cache
 
 def infidelity(a,b):
     return (1-qutip.fidelity(qutip.Qobj(a), qutip.Qobj(b)),qutip.tracedist(1e4*qutip.Qobj(a), 1e4*qutip.Qobj(b))/1e4)
 
+@lru_cache(1000)
+def _integrate(*args):
+    return sp.integrate(*args)
+
+def seg_integrate(f_l,x,interval_l,eval_flag):
+    if eval_flag == False:
+        res = 0
+        for i in range(len(f_l)-1):
+            res += _integrate(f_l[i],(x,interval_l[i][0],interval_l[i][1])).evalf()
+        i = len(f_l)-1
+        res += _integrate(f_l[i],(x,interval_l[i][0],interval_l[i][1]))
+        #res = sum([_integrate(f_l[i],(x,interval_l[i][0],interval_l[i][1])) for i in range(len(f_l))])
+        return res
+    else:
+        return sum([_integrate(f_l[i],(x,interval_l[i][0],interval_l[i][1])).evalf() for i in range(len(f_l))])
 
 
 def test_N_ions_constant():
@@ -118,9 +135,10 @@ def test_N_ions_syc():
 
         para.amplitude = syc.get_amp()
         '''
-      
-        para.t_tol, para.amplitude = read_amp.get(para.N_ions,para.j_ions)
-
+        
+        my_amp = read_data.read_data('./results/j01/amp_opt_28.csv')
+        para.t_tol, para.amplitude,_ , __= my_amp.get(para.N_ions,para.j_ions)
+        para.t_tol *= us
         para.Z_freqcal = [(x - para.mu)*MHz for x in para.Z_freqcal]
         
 
@@ -148,109 +166,6 @@ def test_N_ions_syc():
         print('--------')
         print('N_ions',N_ions,'cut_off',para.cut_off,'(infid,tracedist)',infid1)
 
-
-def test_N_ions_hzx():
-
-    #construct para
-    def p(N_ions,lind_type):
-    
-        para = trap_config.trap_para()
-
-        # basic parameters
-        para.N_ions = N_ions
-        para.j_ions = [0,1]
-        para.omega_ax = 0.32*2*np.pi*MHz
-        para.omega_ra = 2.18*2*np.pi*MHz
-        para.cut_off = 10
-        para.heating_rate = 1e2
-        para.lamb_dicke = 0.1
-        para.mu = 10*kHz
-        para.t_tol = 100*us
-
-        # set initial state
-        para._ini_state = (qutip.tensor(qutip.fock_dm(2,0),qutip.fock_dm(2,0)))
-        para._phonon_ini = (qutip.fock_dm(para.cut_off,0))
-        para.lind_type = lind_type
-
-        # compute phonon frequency and bij
-        phonons = trap_config.Ion_Trap_phonon(para.N_ions, para.omega_ax, para.omega_ra)
-        para.Z_freqcal, para.Z_modes = phonons.w, phonons.b
-        para.partialpos = phonons.equ_pos()
-
-
-        # compute amplitute: chi_j1 chi_j2
-        a = hzx_amp(N_ions = N_ions,Omega_ax = para.omega_ax,Omega_ra = para.omega_ra,target_phase = np.pi/4,j_list = para.j_ions,N_seg = 7,tau = para.t_tol)
-        a.optimize()
-        amp = a.get_amp()
-        amplitude = {0:amp,1:amp}
-        para.amplitude = amplitude
-
-        
-
-        para.init_2()      
-
-        return para
-
-    for N_ions in range(2,10):
-        
-        para = p(N_ions,['aH','a'])
-        t1 = time.time()
-        alg1_lind = sim_method_2L.alg1(para).sim()
-        print('alg1:',alg1_lind)
-        t2 = time.time()
-        print('with lind','N_ions =',N_ions, 'time alg1 =',t2-t1)
-        para.lind_type = []
-        t1 = time.time()
-        alg1 = sim_method_2L.alg1(para).sim()
-        print('alg1:',alg1)
-        t2 = time.time()
-        print('no lind','N_ions =',N_ions, 'time alg1 =',t2-t1)
-
-        infid1 = infidelity(alg1, alg1_lind)
-        print('--------')
-        print(N_ions,infid1)
-
-
-def test_N_ions_hzx2():
-
-    #construct para
-    def p(N_ions,lind_type):
-        cut_off = 10
-        Omega_R = 2*np.pi*30*MHz
-        heating_rate = 1e2
-        laser_ion_list = [0,1]
-        ini_state= (qutip.tensor(qutip.fock_dm(2,0),qutip.fock_dm(2,0)))    # init condition
-        phonon_ini = (qutip.fock_dm(cut_off,0)) # init of phonon
-        detuning = 20*kHz
-        tau = 100*us
-        a = hzx_amp(N_ions = N_ions,Omega_ax = 0.32*2*np.pi*MHz,Omega_ra = 2.18*2*np.pi*MHz,target_phase = np.pi/4,j_list = laser_ion_list,N_seg = 7,tau = tau)
-        a.optimize()
-        amp = a.get_amp()
-        amplitude = {0:amp,1:amp}
-        #amplitude = {0:lambda t: Omega_R * np.sin(detuning*t),1:lambda t: Omega_R * np.sin(detuning*t)}
-        para = trap_config.trap_para()
-        para .my_init(ion_number = N_ions, laser_ion_list = laser_ion_list,omega_ax = 0.32*2*np.pi*MHz,omega_ra = 2.18*2*np.pi*MHz, cut_off = cut_off,heating_rate = heating_rate,t_tol = tau,lamb_dicke = 0.1,_ini_state=ini_state,_phonon_ini=phonon_ini,basis = 'Gellman',amplitude = amplitude, lind_type = lind_type,if_testXX=False)
-        return para
-
-    
-    for N_ions in range(2,10):
-        
-        para = p(N_ions,['aH','a'])
-        t1 = time.time()
-        alg1_lind = sim_method_2L.alg1(para).sim()
-        print('alg1:',alg1_lind)
-        t2 = time.time()
-        print('with lind','N_ions =',N_ions, 'time alg1 =',t2-t1)
-        para.lind_type = []
-        t1 = time.time()
-        alg1 = sim_method_2L.alg1(para).sim()
-        print('alg1:',alg1)
-        t2 = time.time()
-        print('no lind','N_ions =',N_ions, 'time alg1 =',t2-t1)
-
-        infid1 = infidelity(alg1, alg1_lind)
-        print('--------')
-        print(N_ions,infid1)
 
 def test_optimize_syc():
     #construct para
@@ -326,83 +241,10 @@ def test_optimize_syc():
             else:
                 segNum+=1
        
-def test_N_ions_syc3():
-
-    #construct para
-    def p(N_ions,lind_type,cut_off):
-    
-        para = trap_config.trap_para()
-
-        # basic parameters
-        para.N_ions = N_ions
-        para.j_ions = [0,1]
-        para.omega_ax = 0.32*2*np.pi*MHz
-        para.omega_ra = 2.18*2*np.pi*MHz
-        para.cut_off = cut_off
-        para.heating_rate = 1e2
-        para.lamb_dicke = 0.1
-
-        # set initial state
-        para._ini_state = (qutip.tensor(qutip.fock_dm(2,0),qutip.fock_dm(2,0)))
-        para._phonon_ini = (qutip.fock_dm(para.cut_off,0))
-        para.lind_type = lind_type
-
-        # compute phonon frequency and bij
-        #phonons = trap_config.Ion_Trap_phonon(para.N_ions, para.omega_ax, para.omega_ra)
-        para.partialpos = np.array(range(para.N_ions))*5
-        y=np.array(range(para.N_ions))*5*us
-        para.Z_freqcal, para.Z_modes = radial_mode_spectrum( para.N_ions, para.omega_ax,para.omega_ra,y)
-        #para.partialpos = phonons.equ_pos()
-        para.mu = sum(para.Z_freqcal)/len(para.Z_freqcal)
-        print('Z_freq',para.Z_freqcal)
-        # compute amplitute: chi_j1 chi_j2
-        '''
-        syc = syc_amp(ion_number=para.N_ions,j_list=para.j_ions,omega=[x - para.mu for x in para.Z_freqcal],bij=np.matrix(para.Z_modes).transpose(),tau=para.t_tol/us,segment_num=para.segNum,lamb_dicke=para.lamb_dicke,mu = para.mu)
-
-        syc.func2_optimize_process_save_data(plotfig=False, pulse_symmetry = False, ions_same_amps = True)
-
-        #syc.func3_import_saved_data(plotfig = False, pulse_symmetry = False, ions_same_amps = True)
-
-        syc.import_amp()
-
-        para.amplitude = syc.get_amp()
-        '''
-      
-        para.t_tol, para.amplitude = read_amp.get(para.N_ions,para.j_ions)
-
-        para.Z_freqcal = [(x - para.mu)*MHz for x in para.Z_freqcal]
-        
-
-        para.init_2()      
-
-        return para
-
-    
-    for cut_off in range(12,17):
-
-        N_ions= 3
-        para = p(N_ions,[],cut_off)
-        para.cut_off = cut_off
-        t1 = time.time()
-        #alg1_lind = sim_method_2L.alg1(para).sim()
-        #print('alg1:',alg1_lind)
-        t2 = time.time()
-        #print('with lind','N_ions =',N_ions, 'time alg1 =',t2-t1)
-        #para.lind_type = []
-        t1 = time.time()
-        alg1 = sim_method_2L.alg1(para).sim()
-        print('alg1:',alg1)
-        t2 = time.time()
-        print('no lind','N_ions =',N_ions, 'time alg1 =',t2-t1)
-
-        #infid1 = infidelity(alg1, alg1_lind)
-        print('--------')
-        print('N_ions',N_ions,'cut_off',para.cut_off)
-
 def compute_my_bound():
 
     #construct para
-    def p(N_ions,lind_type):
+    def my_p(N_ions,lind_type):
     
         para = trap_config.trap_para()
 
@@ -443,34 +285,57 @@ def compute_my_bound():
         para.amplitude = syc.get_amp()
         '''
       
-        para.t_tol, para.amplitude, para.segNum = read_amp.get(para.N_ions,para.j_ions)
+        my_amp = read_data.read_data('./results/j01/amp_opt_28.csv')
+        para.t_tol, para.amplitude, para.segNum, para.amp_list, para.detuning  = my_amp.get(para.N_ions,para.j_ions)
 
-        para.Z_freqcal = [(x - para.mu)*MHz for x in para.Z_freqcal]
         
 
-        para.init_2()      
+        #para.init_2()      
 
         return para
 
-    for N_ions in range(3,13):
-        para = p(N_ions,['aH','a'])
-        for j in para.j_ions:
-            
-            def int1_sqr(t,k):
-                real_int = integrate.quad(lambda t0: para.amplitude[j](t0)*np.cos(para.Z_freqcal[k]*t0),0,t,limit = para.segNum*20)
-                complex_int = integrate.quad(lambda t0: para.amplitude[j](t0)*np.sin(para.Z_freqcal[k]*t0),0,t,limit = para.segNum*20)
-                return real_int[0]**2 + complex_int[0]**2
+    for N_ions in range(2,13):
+        para = my_p(N_ions,['aH','a'])
+        ###### for test
+        #para.segNum = 2
+        #para.amp_list = [[1,-1],[1,-1]]
+        #para.detuning = [0,0]
+        #para.t_tol = 2
+        print('N_ions',para.N_ions)
+        print('segNum',para.segNum)
+        print('amp_list',para.amp_list)
+        print('detuning',para.detuning)
+        print('t_tol',para.t_tol)
+        #######
+        for j in para.j_ions[0:1]:
+            delta_t = para.t_tol/para.segNum
+            t = sp.Symbol('t',real = True)
             result = 0
             for k in range(para.N_ions):
-                temp_result = integrate.quad(lambda t: int1_sqr(t,k),0,para.t_tol,limit = para.segNum*20)
-                result = max(result,temp_result[0])
-            result = result* 4*2*para.heating_rate*para.lamb_dicke**2
-            print('N =',N_ions,'my bound',result)
+                t1 = time.time()
+                print('k = ',k)
+                t0 = sp.Symbol('t0',real = True)
+                f_l = []
+                interval_l = [(i*delta_t,(i+1)*delta_t) for i in range(para.segNum)]
+                for p in range(para.segNum):
+                    alpha_int = seg_integrate(np.array(para.amp_list[0][0:p+1])*sp.exp(-sp.I*sp.S(para.detuning[k])*t0),t0,[(i*delta_t,(i+1)*delta_t) for i in range(p)]+[(p*delta_t,t)],eval_flag=False)
+                    alpha_int = sp.expand(alpha_int)  #此处还有优化空间,因为同样的式子被simplify了好多次
+                    alpha_int = alpha_int * sp.conjugate(alpha_int)
+                    alpha_int = sp.expand(alpha_int)
+                    f_l.append(alpha_int)
+                t2 = time.time()
+                print('median',t2-t1)
+                temp_result = seg_integrate(f_l, t, interval_l,eval_flag = True)
+                result = max(result,sp.re(temp_result.evalf()))
+                t3 = time.time()
+                print('end',t3-t2)
+            result = result* 4*2*para.heating_rate*para.lamb_dicke**2*1e-6
+            print('---------------N =',N_ions,'my bound',result)
+
 if __name__ == '__main__':
     
     #test_N_ions_syc()
     #test_optimize_syc()
-    #test_N_ions_syc3()
     compute_my_bound()
 
 
--- results/j01/draw.py
+++ results/j01/draw.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 #by default, .plot has line, not mark;.scatter has mark not line
 N = [2,3,4,5,6,7,8,9,10,11,12]
 heating_error = [0.004497865787334798,0.007936377532300032,0.005723306241093851,0.0045856509409013535,0.006050634291640988,0.017035091592401685,0.008318783619748582,0.004568849766719119,0.003949038258522886,0.03109336752259062,0.033921590429074944]
-my_bound = [0.018103753598240485,]
+my_bound = [0.018103753598240485,0.04407204644914977,0.04676122614286645,0.0462891573015059,0.0666125770363947,0.128566577650382,0.0787168502853161,0.0554046786881606,0.0498593459494195,0.426612935618896,0.512697444458948]
 tau = [150,270,300,300,375,480,540,540,570,720,780]
 plt.plot(N,[heating_error[i]/tau[i] for i in range(len(heating_error))],label = 'numerical result')
-plt.plot(N,[2*N[i]*1e2*1e-6 for i in range(len(heating_error))],label = 'N$\Gamma t$')
-#plt.plot(N,[8/3*(0.5*tau[i])**2*1e-6 for i in range(len(heating_error))],label = 'our new bound$')
-plt.xlabel('x axis')
-plt.ylabel('y axis')
-plt.title('infid-N (t normalized)')
+#plt.plot(N,[2*N[i]*1e2*1e-6 for i in range(len(heating_error))],label = 'N$\Gamma t$')
+plt.plot(N[0:len(my_bound)],[my_bound[i]/tau[i] for i in range(len(my_bound))],label = 'our new bound eq.(32)')
+plt.xlabel('N')
+plt.ylabel('infid/t($\mu s$)')
+plt.title('infid-N (t normalized) (0 1 ions illuminated)')
+plt.legend(loc = 'upper left')
 plt.show()
